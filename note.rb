201b233df1f7d4962f879f4f7f0b09184ab006d2
given <givensuman@gmail.com>
2024-07-31 22:27:24 +0000

    start thinking about intervals

--- note.rb
+++ note.rb
@@ -12,7 +12,7 @@ class Note
   attr_accessor :name, :opts
 
   def initialize(name, opts = { is_sharp: false, is_flat: false })
-    @name = name
+    @name = name.upcase
     @opts = opts
   end
 
@@ -24,7 +24,44 @@ class Note
     @opts[:is_flat]
   end
 
+  def increase_interval(interval)
+    notes = @opts[:is_sharp] ? NOTES_W_SHARPS : NOTES_W_FLATS
+  end
+
   def to_s
     "#{name}#{sharp? ? '#' : nil}#{flat? ? 'b' : nil}"
   end
 end # class Note
+
+NOTES_W_FLATS = [ 
+  Note.new('A'),
+  Note.new('B', { :is_flat }),
+  Note.new('B'),
+  Note.new('C'),
+  Note.new('D', { :is_flat }),
+  Note.new('D'),
+  Note.new('E', { :is_flat }),
+  Note.new('E'),
+  Note.new('F'),
+  Note.new('G', { :is_flat }),
+  Note.new('G'),
+  Note.new('A', { :is_flat }),
+]
+
+NOTES_W_SHARPS = [ 
+  Note.new('A'),
+  Note.new('A', { :is_sharp }),
+  Note.new('B'),
+  Note.new('C'),
+  Note.new('C', { :is_sharp }),
+  Note.new('D'),
+  Note.new('D', { :is_sharp }),
+  Note.new('E'),
+  Note.new('F'),
+  Note.new('F', { :is_sharp }),
+  Note.new('G'),
+  Note.new('G', { :is_sharp }),
+]
+
+note = Note.new("C")
+note.increase_interval 6
